484c882cb61ccb1b0f39c73df5a671b3a158ba8e
brandon03-cell <brandonchusgo2003@gmail.com>
2026-05-12 13:45:13 +0200

    insertarSocio(Socio socio) done

--- src/dao/SocioDAO.java
+++ src/dao/SocioDAO.java
@@ -1,4 +1,21 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import modelo.Socio;
+
 public class SocioDAO {
+    private EntityManagerFactory emf;
+
+    public SocioDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public void insertarSocio(Socio socio) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(socio);
+        em.getTransaction().commit();
+        em.close();
+    }
 }
