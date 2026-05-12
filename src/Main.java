8ab7c7c22e63993e9caebbd02de44cc7cb3c1472
brandon03-cell <brandonchusgo2003@gmail.com>
2026-05-12 14:42:26 +0200

    doing main ldsakhdjlaksjd

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+import dao.GimnasioDAO;
+import dao.SocioDAO;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import modelo.Gimnasio;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Gimnasio.odb");
+        GimnasioDAO gymDAO = new GimnasioDAO(emf);
+        Gimnasio gymProMax4Kxd = new Gimnasio("Gimnasio Franquista", "CEI", 67.67);
     }
 }
