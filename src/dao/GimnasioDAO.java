fc96d5a994520203d2ec26e161693db9f6e0ceff
brandon03-cell <brandonchusgo2003@gmail.com>
2026-05-13 09:04:01 +0200

    añadir private al emf

--- src/dao/GimnasioDAO.java
+++ src/dao/GimnasioDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 public class GimnasioDAO {
-    EntityManagerFactory emf;
+    private EntityManagerFactory emf;
 
     public GimnasioDAO(EntityManagerFactory emf) {
         this.emf = emf;
